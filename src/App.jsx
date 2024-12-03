912a9fdf77f4a802d807d5f4a2a9deba57811041
Arthur Smielewski <arthur.smielewski@gmail.com>
2024-12-03 22:02:29 +0000

    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const pokemonAPI = 'https://pokeapi.co/api/v2/pokemon';
+
+async function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(1);
+
+  const [pokemonSprite, setSprite] = useState();
+  console.log(pokemonSprite);
+
+  const [pokemonName, setName] = useState();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchAPI(pokemonID) {
+    try {
+      setIsLoading(true);
+      const response = await fetch(`${pokemonAPI}/${pokemonID}`);
+      await delay(1000);
+      const data = await response.json ();
+      const sprite = data.sprites.front_default;
+      const name = data.name;
+      setName(name.toUpperCase());
+      setSprite(sprite);
+      setIsLoading(false);
+
+      console.log(data);
+    }
+    catch(error) {
+      console.error(error);
+    }
+  }
+
+  function callThis() {
+    console.log('useEffect ran', count)
+    fetchAPI(count);
+  }
+
+  useEffect(callThis, [count]);
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      
       </div>
-      <h1>Vite + React</h1>
+      <h1>Pokemon Picker In React</h1>
       <div className="card">
+        {isLoading && <><p>Loading...</p></>}
+        {!isLoading && pokemonName && <><h3>{pokemonName}</h3></>}
+        {!isLoading && pokemonSprite && <><img src={pokemonSprite}/><br/></>}
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -29,7 +64,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
